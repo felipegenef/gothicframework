06cdc322cd0c5a10ebfac912b3fab7f49c7d189d
felipegenef <felipegenef@gmail.com>
2026-05-21 18:58:34 -0300

    v2.16.0: bump version

--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var CURRENT_VERSION string = "v2.12.1"
+var CURRENT_VERSION string = "v2.16.0"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
